3bed43801f29f45029480e7f5bd690ac8e1590b3
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2024-10-09 13:30:49 -0400

    more flake

--- flake.nix
+++ flake.nix
@@ -6,15 +6,15 @@
   };
 
   outputs = {self, nixpkgs }: {
-    packages.x86_64-darwin.default = nixpkgs.86_64-darwin.rPackages.buildRPackage {
+    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.rPackages.buildRPackage {
       name = "rdrop2";
-      src = nixpkgs.legacyPackages.x86_64-linux.pkgs.fetchgit {
+      src = fetchGit {
         name = "rdrop2-source";
         url = "git+ssh://krzysztof@fawkes.io:2222/data/git/git/rdrop2?shallow=1";
         rev = "3e617bea72ed0d53a8d4f738f0efc90ed77f2548";
-        sha256 = "sha256-5rLdk4ZNyueWqBC0KzLaZAd0QO2cgtWIJvzHcSjwoUA=";
+#        sha256 = "sha256-5rLdk4ZNyueWqBC0KzLaZAd0QO2cgtWIJvzHcSjwoUA=";
       };
-      propagatedBuildInputs = with nixpkgs.legacyPackages.x86_64-darwin.rPackages; [
+      propagatedBuildInputs = with nixpkgs.legacyPackages.x86_64-linux.rPackages; [
         digest
         dplyr
         httr
