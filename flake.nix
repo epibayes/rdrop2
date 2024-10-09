5518b64ff40adc4cfe5b306d4b5d6167e74ec910
Krzysztof Sakrejda <krzysztof.sakrejda@gmail.com>
2024-10-09 16:19:47 -0400

    Flake update

--- flake.nix
+++ flake.nix
@@ -10,7 +10,8 @@
       name = "rdrop2";
       src = fetchGit {
         name = "rdrop2-source";
-        url = "git+ssh://krzysztof@fawkes.io:2222/data/git/git/rdrop2?shallow=1";
+        url = "git+ssh://git@fawkes.io:2222/data/git/git/rdrop2";
+        ref = "main";
         rev = "3e617bea72ed0d53a8d4f738f0efc90ed77f2548";
 #        sha256 = "sha256-5rLdk4ZNyueWqBC0KzLaZAd0QO2cgtWIJvzHcSjwoUA=";
       };
